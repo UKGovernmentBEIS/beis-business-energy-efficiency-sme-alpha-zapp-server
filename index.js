cce2da12e9b184c37b5c996b8a82b8f039804f8e
Jamie Humphries <jamie.humphries@softwire.com>
2018-06-12 17:38:57 +0100

    Wire up development environment with linting, debugging, etc.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
-  socket.on('disconnect', () => console.log('user disconnected'))
-});
+  console.log('CONNECTED')
+  socket.on('disconnect', () => console.log('DISCONNECTED'))
+  socket.on('network', network => console.log(`NETWORK: ${network}`))
+})
 
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5000
 http.listen(port, () => console.log(`Listening on port ${port}.`))
