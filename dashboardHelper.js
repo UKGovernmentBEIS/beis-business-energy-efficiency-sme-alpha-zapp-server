f63656747852792017190c03bce645e9d41d3ec7
Raam Chauhan <raam.chauhan@softwire.com>
2018-07-30 11:20:45 +0100

    Move SQL queries into dashboardHelper.

---
+++ dashboardHelper.js
@@ -0,0 +1,268 @@
+const { Client } = require('pg')
+
+module.exports = {
+  getDashboardData: async function (companyName) {
+    const totalUsers = await getTotalUsersData(companyName)
+    const labels = [...Array(totalUsers.length).keys()].map(k => `${k + 1}`)
+
+    const cumulativeData = await getCumulativeData(companyName)
+    const dailyUserData = await getDailyUserData(companyName, totalUsers)
+    const installationData = await getInstallationData(companyName)
+    const optedInData = await getOptedInData(companyName, totalUsers)
+    const heatingNotificationData = await getHeatingNotificationData(companyName)
+    return {
+      labels: JSON.stringify(labels),
+      cumulativeData,
+      dailyUserData,
+      installationData,
+      optedInData,
+      heatingNotificationData
+    }
+  },
+
+  query: async function (queryTextOrConfig, values) {
+    const client = getPgClient()
+    await client.connect()
+    const result = await client.query(queryTextOrConfig, values)
+    await client.end()
+    return result
+  }
+}
+
+async function getCumulativeData (companyName) {
+  const zappHibernations = await getCumulativeDataByAction(companyName, 'ZappHibernation')
+  const heatingActions = await getCumulativeDataByAction(companyName, 'HeatingFirstLoginDone')
+  return {
+    zappHibernations,
+    heatingActions
+  }
+}
+
+async function getCumulativeDataByAction (companyName, action) {
+  const data = await query(`SELECT 
+  company.name, action.code
+  FROM action_log
+  INNER JOIN company ON action_log.company_id = company.id
+  INNER JOIN action ON action_log.action_id = action.id
+  WHERE company.name ILIKE $1
+  AND action.code ILIKE $2;`, [companyName, action])
+  return data.rowCount
+}
+
+async function getDailyUserData (companyName, totalUsers) {
+  const activeUsers = await getActiveUsersData(companyName)
+
+  return {
+    totalUsers: JSON.stringify(totalUsers.map(d => d.totalusers)),
+    activeUsers: JSON.stringify(activeUsers.map(d => d.activeusers))
+  }
+}
+
+async function getActiveUsersData (companyName) {
+  const data = await query(`SELECT datetable.date, COALESCE(activeuserstable.activeusers, 0) AS activeusers 
+  FROM 
+  
+  (
+    select i::date AS date from generate_series(
+    (
+    SELECT timestamp::date FROM action_log
+    ORDER BY timestamp ASC
+    LIMIT 1
+    ), 
+      current_date, '1 day'::interval) i
+      WHERE (EXTRACT(ISODOW FROM i) < 6)
+  ) AS datetable 
+  
+  LEFT JOIN 
+  
+  (
+    SELECT activedaystable.date, COUNT(pseudonym) AS activeusers
+      FROM (
+        SELECT DISTINCT  a1.pseudonym,  a1."timestamp"::date AS date
+        FROM action_log AS a1
+        INNER JOIN company ON a1.company_id = company.id
+        LEFT JOIN action_log a2 On a1.pseudonym = a2.pseudonym AND a1.timestamp::date = a2.timestamp::date AND a2.action_id = 14
+        WHERE a2.id IS NULL
+        AND (EXTRACT(ISODOW FROM a1."timestamp") < 6)
+        AND company.name ILIKE $1
+        ) AS activedaystable
+    GROUP BY activedaystable.date
+    ORDER BY activedaystable.date
+  ) AS activeuserstable
+  
+  ON datetable.date = activeuserstable.date
+  
+  ;`, [companyName])
+  return data.rows
+}
+
+async function getTotalUsersData (companyName) {
+  const data = await query(`
+  SELECT datetable.date, COUNT(installtable.pseudonym) AS totalusers FROM 
+
+(
+select i::date AS date from generate_series(
+    (
+    SELECT timestamp::date FROM action_log
+    ORDER BY timestamp ASC
+    LIMIT 1
+    ), 
+  current_date, '1 day'::interval) i
+WHERE (EXTRACT(ISODOW FROM i) < 6)
+) AS datetable 
+LEFT JOIN 
+(SELECT  a1.pseudonym,  a1."timestamp"::date AS installdate, a2."timestamp"::date AS uninstalldate
+FROM action_log AS a1
+LEFT JOIN action_log AS a2 ON a1.pseudonym = a2.pseudonym AND a2.action_id = 14
+INNER JOIN company ON a1.company_id = company.id
+WHERE a1.action_id = 13 AND company.name = $1) AS installtable
+ON installtable.installdate <= datetable.date AND (installtable.uninstalldate > datetable.date OR installtable.uninstalldate IS NULL)
+GROUP BY datetable.date
+ORDER BY datetable.date ASC;`, [companyName])
+  return data.rows
+}
+
+async function getInstallationData (companyName) {
+  const data = await query(`
+  SELECT datetable.date, COALESCE(a1.installs, 0) AS installs, COALESCE(a1.uninstalls, 0) AS uninstalls
+  FROM  
+  (
+    select i::date AS date from generate_series(
+    (
+    SELECT timestamp::date FROM action_log
+    ORDER BY timestamp ASC
+    LIMIT 1
+    ), 
+      current_date, '1 day'::interval) i
+      WHERE (EXTRACT(ISODOW FROM i) < 6)
+  ) AS datetable 
+  LEFT JOIN (
+  
+    SELECT timestamp::date AS date, COUNT(NULLIF(action_id = 13, FALSE)) AS installs, COUNT(NULLIF(action_id = 14, FALSE)) AS uninstalls
+    FROM action_log
+    INNER JOIN company ON action_log.company_id = company.id
+    WHERE (EXTRACT(ISODOW FROM "timestamp") < 6)
+    AND company.name ILIKE $1
+    GROUP BY (date)
+    ORDER BY (date)
+  ) AS a1
+  
+  ON datetable.date = a1.date;`, [companyName])
+
+  const installations = JSON.stringify(data.rows.map(d => d.installs))
+  const uninstallations = JSON.stringify(data.rows.map(d => d.uninstalls))
+  return {
+    installations,
+    uninstallations
+  }
+}
+
+async function getOptedInData (companyName, totalUsers) {
+  let optedInHibernationData = await getOptedInDataByActionId(companyName, 7, 8)
+  let optedInHeatingData = await getOptedInDataByActionId(companyName, 9, 10)
+
+  optedInHibernationData = optedInHibernationData.map(d => parseInt(d.optedinusers))
+  optedInHeatingData = optedInHeatingData.map(d => parseInt(d.optedinusers))
+  totalUsers = totalUsers.map(d => parseInt(d.totalusers))
+
+  const optedInHibernationPercentages = calculateAsPercentageOfTotalUsers(optedInHibernationData, totalUsers)
+  const optedInHeatingPercentages = calculateAsPercentageOfTotalUsers(optedInHeatingData, totalUsers)
+
+  return {
+    optedInHibernationPercentages: JSON.stringify(optedInHibernationPercentages),
+    optedInHeatingPercentages: JSON.stringify(optedInHeatingPercentages)
+  }
+}
+
+function calculateAsPercentageOfTotalUsers (values, totalUsers) {
+  return totalUsers.map((t, i) => (t === 0) ? 0 : (100 * (values[i] / t)).toFixed(1))
+}
+
+async function getOptedInDataByActionId (companyName, optInActionId, optOutActionId) {
+  const data = await query(`SELECT datetable.date, COUNT(optintable.pseudonym) AS optedinusers FROM 
+
+  (
+    select i::date AS date from generate_series(
+    (
+    SELECT timestamp::date FROM action_log
+    ORDER BY timestamp ASC
+    LIMIT 1
+    ), 
+      current_date, '1 day'::interval) i
+      WHERE (EXTRACT(ISODOW FROM i) < 6)
+  ) AS datetable 
+  
+  
+  LEFT JOIN 
+  
+  (SELECT pseudonym, optindate, MIN(optoutdate) AS optoutdate FROM 
+  
+    (SELECT  a1.pseudonym,  a1."timestamp" AS optindate, a2."timestamp" AS optoutdate
+    FROM action_log AS a1
+    LEFT JOIN action_log AS a2 ON a1.pseudonym = a2.pseudonym AND a2.action_id IN ($3,14) AND a1."timestamp" < a2."timestamp"
+  
+    INNER JOIN company ON a1.company_id = company.id
+    WHERE a1.action_id = $2 AND company.name ILIKE $1) AS dateranges
+  
+  
+  GROUP BY pseudonym, optindate) 
+  
+  AS optintable
+  
+  ON optintable.optindate <= datetable.date + interval '1 day' AND (optintable.optoutdate > datetable.date + interval '1 day' OR optintable.optoutdate IS NULL)
+  
+  GROUP BY datetable.date
+  ORDER BY datetable.date ASC 
+  ;`, [companyName, optInActionId, optOutActionId])
+  return data.rows
+}
+
+async function getHeatingNotificationData (companyName) {
+  const data = await query(`
+  SELECT datetable.date, COALESCE(a1.clickedDone, 0) AS clickeddone, COALESCE(a1.clickedNotNow, 0) AS clickednotnow 
+  FROM  
+  (
+    select i::date AS date from generate_series(
+    (
+    SELECT timestamp::date FROM action_log
+    ORDER BY timestamp ASC
+    LIMIT 1
+    ), 
+      current_date, '1 day'::interval) i
+      WHERE (EXTRACT(ISODOW FROM i) < 6)
+  ) AS datetable 
+  LEFT JOIN (
+  
+    SELECT timestamp::date AS date, COUNT(NULLIF(action_id = 15, FALSE)) AS clickedDone, COUNT(NULLIF(action_id = 16, FALSE)) AS clickedNotNow
+    FROM action_log
+    INNER JOIN company ON action_log.company_id = company.id
+    WHERE (EXTRACT(ISODOW FROM "timestamp") < 6)
+    AND company.name ILIKE $1
+    GROUP BY (date)
+    ORDER BY (date)
+  ) AS a1
+  
+  ON datetable.date = a1.date;`, [companyName])
+  const clickedDone = data.rows.map(d => parseInt(d.clickeddone))
+  const clickedNotNow = data.rows.map(d => parseInt(d.clickednotnow))
+  const total = clickedDone.map((d, i) => d + clickedNotNow[i])
+  const heatingNotificationData = total.map((d, i) => (d === 0) ? 0 : (100 * (clickedDone[i] / d)).toFixed(1))
+  return {
+    heatingNotificationData: JSON.stringify(heatingNotificationData)
+  }
+}
+
+async function query (queryTextOrConfig, values) {
+  const client = getPgClient()
+  await client.connect()
+  const result = await client.query(queryTextOrConfig, values)
+  await client.end()
+  return result
+}
+
+function getPgClient () {
+  return new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.POSTGRES_USE_SSL === 'yes'
+  })
+}
--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser')
 const csv = require('csv-express')
 const express = require('express')
 const basicAuth = require('express-basic-auth')
+const dashboardHelper = require('./dashboardHelper')
 const flash = require('express-flash')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const enforce = require('express-sslify')
-const { Client } = require('pg')
 const request = require('request')
 
 const app = express()
@@ -48,14 +48,24 @@ app.get('/', (req, res) => {
 })
 
 app.get('/admin', auth, async (req, res) => {
-  const result = await query('SELECT * FROM company;')
+  const result = await dashboardHelper.query('SELECT * FROM company;')
   const messages = req.flash()
   res.render('admin', { companies: result.rows, error: messages.error })
 })
 
+app.get('/admin/company/:code', async (req, res) => {
+  const { code } = req.params
+  const company = await getCompany(code)
+  const dashboardData = await dashboardHelper.getDashboardData(company.name)
+  res.render('company', {
+    name: company.name,
+    dashboardData
+  })
+})
+
 app.post('/admin/company/:code/delete', auth, async (req, res) => {
   const { code } = req.params
-  await query('DELETE FROM company WHERE code = $1', [code])
+  await dashboardHelper.query('DELETE FROM company WHERE code = $1', [code])
   res.redirect('/admin')
 })
 
@@ -63,7 +73,7 @@ app.post('/admin/company/new', auth, async (req, res) => {
   const { code, name } = req.body
   if (code && name) {
     try {
-      await query('INSERT INTO company (code, name) VALUES ($1, $2);', [code, name])
+      await dashboardHelper.query('INSERT INTO company (code, name) VALUES ($1, $2);', [code, name])
     } catch (err) {
       console.warn(err)
       req.flash('error', `Could not add company '${name}' with code '${code}'. Please ensure that both the name and code are unique.`)
@@ -132,7 +142,7 @@ io.on('connection', socket => {
 
     if (company.id && userPseudonym && actionId) {
       try {
-        await query(`INSERT INTO action_log(company_id, pseudonym, action_id)
+        await dashboardHelper.query(`INSERT INTO action_log(company_id, pseudonym, action_id)
         VALUES ($1, $2, $3);`, [company.id, userPseudonym, actionId])
       } catch (err) {
         console.warn(err)
@@ -152,18 +162,18 @@ io.on('connection', socket => {
 })
 
 async function getCompany (code) {
-  const result = await query('SELECT * FROM company WHERE code = $1;', [code])
+  const result = await dashboardHelper.query('SELECT * FROM company WHERE code = $1;', [code])
   return result.rows[0]
 }
 
 async function getActionId (code) {
-  const result = await query('SELECT id FROM action WHERE code = $1;', [code])
+  const result = await dashboardHelper.query('SELECT id FROM action WHERE code = $1;', [code])
   const row = result.rows[0]
   return row ? row.id : null
 }
 
 async function getAllDataForDownload (code) {
-  return query(`SELECT 
+  return dashboardHelper.query(`SELECT 
     to_char(timestamp::date,'DD/MM/YYYY') AS date,  
     timestamp::time(0) AS time,  
     company.name AS company, 
@@ -176,7 +186,7 @@ async function getAllDataForDownload (code) {
 }
 
 async function getCompanyDataForDownload (companyName) {
-  return query(`SELECT 
+  return dashboardHelper.query(`SELECT 
     to_char(timestamp::date,'DD/MM/YYYY') AS date,  
     timestamp::time(0) AS time,  
     company.name AS company, 
@@ -187,277 +197,6 @@ async function getCompanyDataForDownload (companyName) {
   INNER JOIN company ON action_log.company_id = company.id
   INNER JOIN action ON action_log.action_id = action.id
   WHERE company.name ILIKE $1;`, [companyName])
-}
-
-app.get('/admin/company/:code', async (req, res) => {
-  const { code } = req.params
-  const company = await getCompany(code)
-  const cumulativeData = await generateCumulativeData(company.name)
-  const activeUsersData = await getDataForActiveUsers(company.name)
-  const totalUsersData = await getDataForTotalUsers(company.name)
-  const installsChartData = await getDataForInstallsChart(company.name)
-  const heatingNotificationData = await getDataForHeatingNotifications(company.name)
-  const optInHibernationData = await getHibernationOptInData(company.name)
-  const optInHeatingData = await getHeatingOptInData(company.name)
-  const optInHibernationPercentages = calculateAsPercentageOfTotalUsers(optInHibernationData.map(d => parseInt(d.optedinusers)), totalUsersData.map(d => parseInt(d.totalusers)))
-  const optInHeatingPercentages = calculateAsPercentageOfTotalUsers(optInHeatingData.map(d => parseInt(d.optedinusers)), totalUsersData.map(d => parseInt(d.totalusers)))
-  const labels = [...Array(installsChartData.length).keys()].map(k => `${k + 1}`)
-  res.render('company', {
-    name: company.name,
-    cumulativeData,
-    labels: JSON.stringify(labels),
-    activeUsersDate: JSON.stringify(activeUsersData.map(d => d.activeusers)),
-    totalUsersData: JSON.stringify(totalUsersData.map(d => d.totalusers)),
-    installsData: JSON.stringify(installsChartData.map(d => d.installs)),
-    uninstallsData: JSON.stringify(installsChartData.map(d => d.uninstalls)),
-    heatingNotificationData: JSON.stringify(heatingNotificationData),
-    optInHibernationPercentages: JSON.stringify(optInHibernationPercentages),
-    optInHeatingPercentages: JSON.stringify(optInHeatingPercentages)
-  })
-})
-
-async function generateCumulativeData (companyName) {
-  const zappHibernations = await getNumbersForAction(companyName, 'ZappHibernation')
-  const heatingClickedDone = await getNumbersForAction(companyName, 'HeatingFirstLoginDone')
-  return {
-    zappHibernations,
-    heatingClickedDone
-  }
-}
-
-async function getNumbersForAction (companyName, action) {
-  const data = await query(`SELECT 
-  company.name, action.code
-  FROM action_log
-  INNER JOIN company ON action_log.company_id = company.id
-  INNER JOIN action ON action_log.action_id = action.id
-  WHERE company.name ILIKE $1
-  AND action.code ILIKE $2;`, [companyName, action])
-  return data.rowCount
-}
-
-async function getDataForActiveUsers (companyName) {
-  const data = await query(`SELECT datetable.date, COALESCE(activeuserstable.activeusers, 0) AS activeusers 
-  FROM 
-  
-  (
-    select i::date AS date from generate_series(
-    (
-    SELECT timestamp::date FROM action_log
-    ORDER BY timestamp ASC
-    LIMIT 1
-    ), 
-      current_date, '1 day'::interval) i
-      WHERE (EXTRACT(ISODOW FROM i) < 6)
-  ) AS datetable 
-  
-  LEFT JOIN 
-  
-  (
-    SELECT activedaystable.date, COUNT(pseudonym) AS activeusers
-      FROM (
-        SELECT DISTINCT  a1.pseudonym,  a1."timestamp"::date AS date
-        FROM action_log AS a1
-        INNER JOIN company ON a1.company_id = company.id
-        LEFT JOIN action_log a2 On a1.pseudonym = a2.pseudonym AND a1.timestamp::date = a2.timestamp::date AND a2.action_id = 14
-        WHERE a2.id IS NULL
-        AND (EXTRACT(ISODOW FROM a1."timestamp") < 6)
-        AND company.name ILIKE $1
-        ) AS activedaystable
-    GROUP BY activedaystable.date
-    ORDER BY activedaystable.date
-  ) AS activeuserstable
-  
-  ON datetable.date = activeuserstable.date
-  
-  ;`, [companyName])
-  return data.rows
-}
-
-async function getDataForTotalUsers (companyName) {
-  const data = await query(`
-  SELECT datetable.date, COUNT(installtable.pseudonym) AS totalusers FROM 
-
-(
-select i::date AS date from generate_series(
-    (
-    SELECT timestamp::date FROM action_log
-    ORDER BY timestamp ASC
-    LIMIT 1
-    ), 
-  current_date, '1 day'::interval) i
-WHERE (EXTRACT(ISODOW FROM i) < 6)
-) AS datetable 
-LEFT JOIN 
-(SELECT  a1.pseudonym,  a1."timestamp"::date AS installdate, a2."timestamp"::date AS uninstalldate
-FROM action_log AS a1
-LEFT JOIN action_log AS a2 ON a1.pseudonym = a2.pseudonym AND a2.action_id = 14
-INNER JOIN company ON a1.company_id = company.id
-WHERE a1.action_id = 13 AND company.name = $1) AS installtable
-ON installtable.installdate <= datetable.date AND (installtable.uninstalldate > datetable.date OR installtable.uninstalldate IS NULL)
-GROUP BY datetable.date
-ORDER BY datetable.date ASC;`, [companyName])
-  return data.rows
-}
-
-async function getDataForInstallsChart (companyName) {
-  const data = await query(`
-  SELECT datetable.date, COALESCE(a1.installs, 0) AS installs, COALESCE(a1.uninstalls, 0) AS uninstalls
-  FROM  
-  (
-    select i::date AS date from generate_series(
-    (
-    SELECT timestamp::date FROM action_log
-    ORDER BY timestamp ASC
-    LIMIT 1
-    ), 
-      current_date, '1 day'::interval) i
-      WHERE (EXTRACT(ISODOW FROM i) < 6)
-  ) AS datetable 
-  LEFT JOIN (
-  
-    SELECT timestamp::date AS date, COUNT(NULLIF(action_id = 13, FALSE)) AS installs, COUNT(NULLIF(action_id = 14, FALSE)) AS uninstalls
-    FROM action_log
-    INNER JOIN company ON action_log.company_id = company.id
-    WHERE (EXTRACT(ISODOW FROM "timestamp") < 6)
-    AND company.name ILIKE $1
-    GROUP BY (date)
-    ORDER BY (date)
-  ) AS a1
-  
-  ON datetable.date = a1.date;`, [companyName])
-  return data.rows
-}
-
-async function getHibernationOptInData (companyName) {
-  const data = await query(`SELECT datetable.date, COUNT(optintable.pseudonym) AS optedinusers FROM 
-
-  (
-    select i::date AS date from generate_series(
-    (
-    SELECT timestamp::date FROM action_log
-    ORDER BY timestamp ASC
-    LIMIT 1
-    ), 
-      current_date, '1 day'::interval) i
-      WHERE (EXTRACT(ISODOW FROM i) < 6)
-  ) AS datetable 
-  
-  
-  LEFT JOIN 
-  
-  (SELECT pseudonym, optindate, MIN(optoutdate) AS optoutdate FROM 
-  
-    (SELECT  a1.pseudonym,  a1."timestamp" AS optindate, a2."timestamp" AS optoutdate
-    FROM action_log AS a1
-    LEFT JOIN action_log AS a2 ON a1.pseudonym = a2.pseudonym AND a2.action_id IN (8,14) AND a1."timestamp" < a2."timestamp"
-  
-    INNER JOIN company ON a1.company_id = company.id
-    WHERE a1.action_id = 7 AND company.name ILIKE $1) AS dateranges
-  
-  
-  GROUP BY pseudonym, optindate) 
-  
-  AS optintable
-  
-  ON optintable.optindate <= datetable.date + interval '1 day' AND (optintable.optoutdate > datetable.date + interval '1 day' OR optintable.optoutdate IS NULL)
-  
-  GROUP BY datetable.date
-  ORDER BY datetable.date ASC 
-  ;`, [companyName])
-  return data.rows
-}
-
-async function getHeatingOptInData (companyName) {
-  const data = await query(`SELECT datetable.date, COUNT(optintable.pseudonym) AS optedinusers FROM 
-
-  (
-    select i::date AS date from generate_series(
-    (
-    SELECT timestamp::date FROM action_log
-    ORDER BY timestamp ASC
-    LIMIT 1
-    ), 
-      current_date, '1 day'::interval) i
-      WHERE (EXTRACT(ISODOW FROM i) < 6)
-  ) AS datetable 
-  
-  
-  LEFT JOIN 
-  
-  (SELECT pseudonym, optindate, MIN(optoutdate) AS optoutdate FROM 
-  
-    (SELECT  a1.pseudonym,  a1."timestamp" AS optindate, a2."timestamp" AS optoutdate
-    FROM action_log AS a1
-    LEFT JOIN action_log AS a2 ON a1.pseudonym = a2.pseudonym AND a2.action_id IN (10,14) AND a1."timestamp" < a2."timestamp"
-  
-    INNER JOIN company ON a1.company_id = company.id
-    WHERE a1.action_id = 9 AND company.name ILIKE $1) AS dateranges
-  
-  
-  GROUP BY pseudonym, optindate) 
-  
-  AS optintable
-  
-  ON optintable.optindate <= datetable.date + interval '1 day' AND (optintable.optoutdate > datetable.date + interval '1 day' OR optintable.optoutdate IS NULL)
-  
-  GROUP BY datetable.date
-  ORDER BY datetable.date ASC 
-  ;`, [companyName])
-  return data.rows
-}
-
-async function getDataForHeatingNotifications (companyName) {
-  const data = await query(`
-  SELECT datetable.date, COALESCE(a1.clickedDone, 0) AS clickeddone, COALESCE(a1.clickedNotNow, 0) AS clickednotnow 
-  FROM  
-  (
-    select i::date AS date from generate_series(
-    (
-    SELECT timestamp::date FROM action_log
-    ORDER BY timestamp ASC
-    LIMIT 1
-    ), 
-      current_date, '1 day'::interval) i
-      WHERE (EXTRACT(ISODOW FROM i) < 6)
-  ) AS datetable 
-  LEFT JOIN (
-  
-    SELECT timestamp::date AS date, COUNT(NULLIF(action_id = 15, FALSE)) AS clickedDone, COUNT(NULLIF(action_id = 16, FALSE)) AS clickedNotNow
-    FROM action_log
-    INNER JOIN company ON action_log.company_id = company.id
-    WHERE (EXTRACT(ISODOW FROM "timestamp") < 6)
-    AND company.name ILIKE $1
-    GROUP BY (date)
-    ORDER BY (date)
-  ) AS a1
-  
-  ON datetable.date = a1.date;`, [companyName])
-  const clickedDone = data.rows.map(d => parseInt(d.clickeddone))
-  const clickedNotNow = data.rows.map(d => parseInt(d.clickednotnow))
-  const total = clickedDone.map((d, i) => d + clickedNotNow[i])
-  const percentages = total.map((d, i) => (d === 0) ? 0 : (100 * (clickedDone[i] / d)).toFixed(1))
-
-  return percentages
-}
-
-function calculateAsPercentageOfTotalUsers (values, totalUsers) {
-  return totalUsers.map((t, i) => (t === 0) ? 0 : (100 * (values[i] / t)).toFixed(1))
-}
-
-async function query (queryTextOrConfig, values) {
-  const client = getPgClient()
-  await client.connect()
-  const result = await client.query(queryTextOrConfig, values)
-  await client.end()
-  return result
-}
-
-function getPgClient () {
-  return new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.POSTGRES_USE_SSL === 'yes'
-  })
 }
 
 function updateCompanyUsersCount (company) {
